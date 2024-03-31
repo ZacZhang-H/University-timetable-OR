0e88790a7f0591337f7453b8800b57553a0fb2e7
ZacZhang-H <71829003+ZacZhang-H@users.noreply.github.com>
2024-03-31 03:46:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,10 @@ from Lecture_for_semster2 import courses2, hours_per_course2, students_per_cours
 def main():
   
   print("-------SEM1 Lecture-----")
-  semster_lectures(
+  semster_lectures(courses1, hours_per_course1, students_per_course1, room_capacities, rooms, weekdays_num, max_lecture_hours)
+  print("-------SEM2 Lecture-----")
+  semster_lectures(courses2, hours_per_course2, students_per_course2, room_capacities, rooms, weekdays_num, max_lecture_hours)
+
+if __name__ == "__main__":
+    main()
+
