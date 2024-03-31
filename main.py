c1bf9cc1f566533acbb6d1b3962e6e850b92d0f8
ZacZhang-H <71829003+ZacZhang-H@users.noreply.github.com>
2024-03-31 03:57:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Lectures import semster_lectures
-from SEM1_lecture_and_rooms import courses1, hours_per_course1, students_per_course1, courses2, hours_per_course2, students_per_course2，room_capacities, rooms, weekdays_num, max_lecture_hours
+from SEM1_lecture_and_rooms import courses1, hours_per_course1, students_per_course1, courses2, hours_per_course2, students_per_course2,room_capacities, rooms, weekdays_num, max_lecture_hours
 
 
 def main():
