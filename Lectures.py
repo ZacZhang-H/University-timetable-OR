930a17f4034f24816687329a422ae5d486f9cc8a
ZacZhang-H <71829003+ZacZhang-H@users.noreply.github.com>
2024-04-01 08:12:45 +0100

    Update Lectures.py

--- Lectures.py
+++ Lectures.py
@@ -35,18 +35,49 @@ def semster_lectures(courses, hours_per_course, students_per_course, room_capaci
     conflicts = calculate_conflict_occurrences(final_timetable)  # Calculates conflicts based on room and start hour
     conflict_occurrences = calculate_conflict_count(final_timetable) 
     print("conflict occurrences:", conflicts,"number of courses that conflict",conflict_occurrences)
-    conflicting_courses = find_conflicting_courses(optimized_timetable)
+    conflicting_courses = find_conflicting_courses(final_timetable)
     print("Conflicting courses:", conflicting_courses)
 
 
-
-
-    
     # Check unassigned courses
     unassigned = check_unassigned_courses(courses, final_timetable)
     if unassigned:
         print("Unassigned Courses after Optimization:", unassigned)
     else:
         print("All courses have been successfully scheduled.")
+
+
+    print("We try to use Particle Swarm Optimization first and then use Simulated Annealing")
+    
+    
+    # Optimize the results of simulated annealing using PSO
+    print("\nStep 1:  using Particle Swarm Optimization (PSO)")
+    pso_timetable = pso_optimize(initial_timetable,hours_per_course, students_per_course, room_capacities, rooms, weekdays_num, max_lecture_hours, num_particles=30, max_iterations=10000, print_frequency=100)
+    print("Optimized class schedule and the number of conflicts：")
+    print_timetable_by_room(pso_timetable, hours_per_course)
+    conflicts = calculate_conflict_occurrences(pso_timetable)  # Calculates conflicts based on room and start hour
+    conflict_occurrences = calculate_conflict_count(pso_timetable) 
+    print("conflict occurrences:", conflicts,"number of courses that conflict",conflict_occurrences)
+    conflicting_courses = find_conflicting_courses(pso_timetable)
+    print("Conflicting courses:", conflicting_courses)
+    
+    # Optimize random schedules using simulated annealing
+    print("\nStep 2: Optimizing Timetable using Simulated Annealing")
+    SA_timetable = simulated_annealing(pso_timetable,courses, hours_per_course, students_per_course, room_capacities, rooms, weekdays_num=5, max_lecture_hours=9, max_iterations=100000)
+    print("Optimized class schedule and the number of conflicts：")
+    print_timetable_by_room(SA_timetable, hours_per_course)
+    conflicts = calculate_conflict_occurrences(SA_timetable)  # Calculates conflicts based on room and start hour
+    conflict_occurrences = calculate_conflict_count(SA_timetable) 
+    print("conflict occurrences:", conflicts,"number of courses that conflict",conflict_occurrences)
+    conflicting_courses = find_conflicting_courses(SA_timetable)
+    print("Conflicting courses:", conflicting_courses)
+
+    unassigned = check_unassigned_courses(courses, SA_timetable)
+    if unassigned:
+        print("Unassigned Courses after Optimization:", unassigned)
+    else:
+        print("All courses have been successfully scheduled.")
+ 
+    
 
 
