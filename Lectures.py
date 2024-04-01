5f67775d13a0f691e15705d18d4ff74e8cb55ec4
ZacZhang-H <71829003+ZacZhang-H@users.noreply.github.com>
2024-04-01 07:16:19 +0100

    Update Lectures.py

--- Lectures.py
+++ Lectures.py
@@ -2,7 +2,7 @@ from random_timetable_generator import generate_random_timetable
 from simulated_annealing import simulated_annealing
 from pso_optimizer import pso_optimize
 from genetic_algorithm_optimizer import genetic_algorithm_optimize
-from utility_functions import print_timetable, calculate_conflicts, check_unassigned_courses, print_unused_classrooms
+from utility_functions import calculate_conflict_occurrences,calculate_conflict_count,check_unassigned_courses,find_conflicting_course,print_timetable_by_room
 
 def semster_lectures(courses, hours_per_course, students_per_course, room_capacities, rooms, weekdays_num, max_lecture_hours):
     #Generate initial schedule randomly
