f75f30d3b6e8e91137bea6e57528040e7ca4f3f4
ZacZhang-H <71829003+ZacZhang-H@users.noreply.github.com>
2024-04-02 21:21:10 +0100

    Update random_timetable_generator.py

--- random_timetable_generator.py
+++ random_timetable_generator.py
@@ -3,22 +3,24 @@ from utility_functions import calculate_conflict_occurrences,calculate_conflict_
 def generate_random_sample(courses, hours_per_course, students_per_course, room_capacities, rooms, weekdays_num, max_lecture_hours):
     timetable = []
     for course in courses:
-        course_scheduled = False  
+        course_scheduled = False
         hours_needed = hours_per_course[course]
-        for day in range(1, weekdays_num + 1):
-            if course_scheduled:
-                break 
-            for room in rooms:
-                if room_capacities[room] < students_per_course[course]:
-                    continue
-                for start_hour in range(1, max_lecture_hours + 1 - hours_needed):  
-                    end_hour = start_hour + hours_needed
+        while not course_scheduled:
+            day = random.randint(1, weekdays_num)
+            room = random.choice(rooms)
+            if room_capacities[room] >= students_per_course[course]:
+                # 确保开始时间加上持续时间不会超过一天中的时间段
+                if hours_needed == 1: # 如果课程只需要1小时
+                    start_hour_range = max_lecture_hours  # 最后一个时间段可以使用
+                else:
+                    start_hour_range = max_lecture_hours - hours_needed + 1
+                # 选择一个随机的开始时间
+                start_hour = random.randint(1, start_hour_range)
+                end_hour = start_hour + hours_needed
+                if end_hour <= max_lecture_hours:  # 再次确认结束时间不会超出限制
                     if is_time_slot_available(timetable, room, day, start_hour, end_hour):
                         timetable.append((course, room, day, start_hour, end_hour))
-                        course_scheduled = True  
-                        break  
-                if course_scheduled:
-                    break  
+                        course_scheduled = True
     return timetable
 
 
